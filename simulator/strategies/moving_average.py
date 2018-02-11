0869a963e1422a866be9f72a7f99bec58b90e9c5
Josh <yangzijiangjosh@gmail.com>
2018-02-11 11:36:51 -0800

    Updated moving average module

--- simulator/strategies/moving_average.py
+++ simulator/strategies/moving_average.py
@@ -1,15 +1,15 @@
 #A module that can calculates moving average of a series of data.
 
-prices = []
+series = []
 
 #Add a data point
 def add(data):
-    global prices
-    prices.append(data)
+    global series
+    series.append(data)
 
 #Calculate the moving average of specific length
 def moving_average(length):
-    global prices
-    length = min(length, len(prices))
-    return sum(prices[len(prices) - length :]) / length
+    global series
+    length = min(length, len(series))
+    return sum(series[len(prices) - length :]) / length
 
